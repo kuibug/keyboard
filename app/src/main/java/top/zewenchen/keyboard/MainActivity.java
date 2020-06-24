979c804ea3c04900918f534a8764ba0c4050493a
Craftsman-PC <zewen_chen.km@foxmail.com>
2020-06-24 15:49:26 +0800

    commit

--- app/src/main/java/top/zewenchen/keyboard/MainActivity.java
+++ app/src/main/java/top/zewenchen/keyboard/MainActivity.java
@@ -78,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
      * 利用 TCP 发送数据的方法
      */
     protected void connectServerWithTCPSocket(String btn_str) {
-        //TODO 有空做一下SSL加密，局域网是信任的但不安全
+        // TODO 局域网是信任的但不安全
         Socket socket;
         try {// 创建一个Socket对象，并指定服务端的IP及端口号
             socket = new Socket(host, 5021);
@@ -103,133 +103,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * 公用的click方法
-     *
-     * @param str 要发送的字符
-     */
-    public void click(final String str) {
-        addText(str);//添加预览
 
-        //新建线程发送TCP数据，socket不能运行再主线程上
+    public void clicked(View view) {
+        Button btn = (Button) view;
+        final String text = btn.getText().toString();
+        addText(text);//添加预览
+
+        //新建线程发送TCP数据
         new Thread(new Runnable() {
             @Override
             public void run() {
-                connectServerWithTCPSocket(str);
+                connectServerWithTCPSocket(text);
             }
         }).start();
     }
-
-    // TODO 将按键ID初始化可以省去以后每次按键都要find的时间和资源
-    //  按键 mode
-    public void click_mode(View view) {
-        //Button btn = (Button) findViewById(R.id.btn_mode);
-        //click(btn.getText().toString());
-        final TextView textView = findViewById(R.id.textView);
-        textView.setText("按键预览");
-    }
-
-    // 按键 /
-    public void click_mult(View view) {
-        Button btn = findViewById(R.id.btn_mult);
-        click(btn.getText().toString());
-    }
-
-    // 按键 *
-    public void click_div(View view) {
-        Button btn = findViewById(R.id.btn_div);
-        click(btn.getText().toString());
-    }
-
-    // 按键 -
-    public void click_sub(View view) {
-        Button btn = findViewById(R.id.btn_sub);
-        click(btn.getText().toString());
-    }
-
-    // 按键 +
-    public void click_add(View view) {
-        Button btn = findViewById(R.id.btn_add);
-        click(btn.getText().toString());
-    }
-
-    // 按键 enter
-    public void click_enter(View view) {
-        Button btn = findViewById(R.id.btn_enter);
-        click(btn.getText().toString());
-    }
-
-    // 按键 .
-    public void click_dot(View view) {
-        Button btn = findViewById(R.id.btn_dot);
-        click(btn.getText().toString());
-    }
-
-    // 按键 DEL
-    public void click_del(View view) {
-        Button btn = findViewById(R.id.btn_del);
-        click(btn.getText().toString());
-    }
-
-    // 按键 0
-    public void click_0(View view) {
-        Button btn = findViewById(R.id.btn_0);
-        click(btn.getText().toString());
-    }
-
-    // 按键 1
-    public void click_1(View view) {
-        Button btn = findViewById(R.id.btn_1);
-        click(btn.getText().toString());
-    }
-
-    // 按键 2
-    public void click_2(View view) {
-        Button btn = findViewById(R.id.btn_2);
-        click(btn.getText().toString());
-    }
-
-    // 按键 3
-    public void click_3(View view) {
-        Button btn = findViewById(R.id.btn_3);
-        click(btn.getText().toString());
-    }
-
-    // 按键 4
-    public void click_4(View view) {
-        Button btn = findViewById(R.id.btn_4);
-        click(btn.getText().toString());
-    }
-
-    // 按键 5
-    public void click_5(View view) {
-        Button btn = findViewById(R.id.btn_5);
-        click(btn.getText().toString());
-    }
-
-    // 按键 6
-    public void click_6(View view) {
-        Button btn = findViewById(R.id.btn_6);
-        click(btn.getText().toString());
-    }
-
-    // 按键 7
-    public void click_7(View view) {
-        Button btn = findViewById(R.id.btn_7);
-        click(btn.getText().toString());
-    }
-
-    // 按键 8
-    public void click_8(View view) {
-        Button btn = findViewById(R.id.btn_8);
-        click(btn.getText().toString());
-    }
-
-    // 按键 9
-    public void click_9(View view) {
-        Button btn = findViewById(R.id.btn_9);
-        click(btn.getText().toString());
-    }
-
-
 }
